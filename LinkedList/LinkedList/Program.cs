cf9b770413ecd4b7d685c3d7edab2d87625c1ef8
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-03-11 02:06:23 -0500

    [Ruchita] Add: Linked list is created

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1 create a simple Linked list 56-30-70
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
