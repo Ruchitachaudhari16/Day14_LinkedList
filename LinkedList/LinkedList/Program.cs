810469864c9571c4c592b6c2c9d05568955f1f2b
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-03-11 02:23:52 -0500

    [Ruchita] Add: 30 added to 70

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -1,14 +1,19 @@
-﻿namespace LinkedList
+﻿using System.Collections.Generic;
+
+namespace LinkedList
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //UC1 create a simple Linked list 56-30-70
+            //UC2 Ability to create Linked List by adding 30 and 56 to 70
             LinkedList list = new LinkedList();
             list.Add(56);
-            list.Add(30);
+            //Here list.add (30) but 30 added to 70 position
+
             list.Add(70);
+            //30 is added to 70
+            list.InserAtParticularPosition(2, 30);
             list.Display();
         }
     }
